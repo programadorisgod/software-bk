dfad6af2b06f84e0c2ce1fd988ddc1f70be32617
david123456858 <juandavidperaltafuentes@gmail.com>
2024-08-23 22:18:46 -0500

    entidades user y payment

--- src/Entity/user/user.ts
+++ src/Entity/user/user.ts
@@ -1,19 +1,30 @@
-export class User {
+import { Entity, Column, PrimaryColumn, BaseEntity } from "typeorm"
+
+//the established inheritance is made for the ease of the methods offered by Typeorm
+@Entity("users")
+export class User extends BaseEntity {
   //the decorators of typeOrm with respect to attributes already designed in the database
-
+  @PrimaryColumn()
   id!: string
-
+    
+  @Column()
   name!: string
 
+  @Column()
   lastName!: string
 
+  @Column()
   email!: string
 
+  @Column()
   phoneNumber!: string
 
+  @Column()
   password!: string
-
+  
+  @Column({ type:'int' })
   age!: number
 
-  faceImage: string | undefined
+  @Column("simple-array")
+  faceImage!: number[]
 }
