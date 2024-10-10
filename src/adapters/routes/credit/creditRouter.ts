1e6184b83689cd9925846f12b40a8ed7c163638f
MSlegro <legromanuel29@gmail.com>
2024-10-10 18:31:32 -0500

    FIX(useCaseCreditRegister)

--- src/adapters/routes/credit/creditRouter.ts
+++ src/adapters/routes/credit/creditRouter.ts
@@ -4,12 +4,14 @@ import { CreditRepository } from "@Repository/credit/repository"
 import { UseCaseCreditRegister } from "@useCases/credit/creditRegister"
 import { BASE_URL } from "@constants/constants"
 import { Router } from "express"
-import { router } from "@config/routerConfig"
 import { CreditDto } from "@Dtos/credit/creditDto"
 import { validateDto } from "@middlewares/validator"
 
 export const CreateRouterCredit = (): Router => {
+  console.log("entro a la ruta")
   const repository = new CreditRepository()
+
+  const router = Router()
 
   const useCaseRegister = new UseCaseCreditRegister(repository)
 
@@ -18,6 +20,7 @@ export const CreateRouterCredit = (): Router => {
     .build()
 
   if(creditController instanceof CreditController) {
+    console.log("entro al if")
     router.post(`${BASE_URL}/credit`, validateDto(CreditDto) , creditController.register)
   } else {
     console.log(`[Error] when building UserController: ${creditController.error} `)
